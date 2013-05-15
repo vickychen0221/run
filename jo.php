7bfc22421b9d7b25c5bed38f8a82b11305fca00b
vicky <va2946@gmail.com>
2013-05-15 21:46:53 +0800

    insert

--- jo.php
+++ jo.php
@@ -23,4 +23,21 @@ background-repeat:no-repeat;}
         資料新增成功
 </font>
 </body>
+<?php
+$link = mysql_connect("127.0.0.1","root","0000");
 
+mysql_select_db("demorun",$link);
+
+$a=$_POST['SEQnumber'];
+$b=$_POST['idnumber'];
+$c=$_POST['TimeArrive'];
+$d=$_POST['SpendTime'];
+$e=$_POST['name'];
+$f=$_POST['sex'];
+$g=$_POST['tpyesofgame'];
+$h=$_POST['Sdate'];
+$sql = "INSERT into demorun(SEQ,ID,ATIME,TIME,NAME,GENDER,CATE,SELECTDATE) Values ('$a','$b','$c','$d','$e','$f','$g','$h')";
+
+mysql_query($sql,$link);
+mysql_close($link);
+?>
